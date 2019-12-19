62a28ed45e75ea0a18dfb24f6dab899e4b5e5ea3
nt780325 <nt780325@GR13-11.esirem-ad.ad.u-bourgogne.fr>
2019-12-19 17:17:52 +0100

    Creating tests

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -15,5 +15,38 @@ public class MarkovTest {
 		assertNotNull(res);
 		System.out.println(res);
 	}
+	
+	@Test // Test a file with one word => should return a null couple
+	public void testRead1WordFile() {
+		Markov m = new Markov();
+		m.readFile("file1Word.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		assertNull(p);
+	}
+	
+	@Test // Test a file with two words => should return a null couple
+	public void testRead2WordsFile() {
+		Markov m = new Markov();
+		m.readFile("file2Words.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		assertNull(p);
+	}
+	
+	@Test // Test a file with three words => should return a not null couple
+	public void testRead3WordsFile() {
+		Markov m = new Markov();
+		m.readFile("file3Words.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		assertNotNull(p);
+	}
 
+	@Test // Test a file with four words => should return a not null couple
+	public void testRead4WordsFile() {
+		Markov m = new Markov();
+		m.readFile("file4Words.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		String res = m.generateText(p, 4);
+		assertNotNull(res);
+		System.out.println(res);
+	}
 }
